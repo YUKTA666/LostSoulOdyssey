fc04bd9b0e7133e1e37b413e78c575bc665dabf9
yuktabehare <yuktabehare96@gmail.com>
2023-09-09 00:29:39 +0530

    Updates

--- src/components/creative_writing/CreativeWriting.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import "./CreativeWriting.css"
-
-export default function CreativeWriting(){
-    return(
-        <section id='creative'>Creative Writing</section>
-    )
-}
